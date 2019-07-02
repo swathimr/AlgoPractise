79f232f30e8e76e45d25456d20ed992dedecb52a
Swathi <swathi6489@gmail.com>
2019-07-02 09:12:28 -0700

    updates

---
+++ src/DP/LongestCommonSubsequence.java
@@ -0,0 +1,24 @@
+package DP;
+
+//time complexity O(mn)
+public class LongestCommonSubsequence {
+
+    private int getLongestCommonSubsequence(String a, String b) {
+        int m= a.length();int n =b.length();
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i=0;i<=m;i++) {
+            for(int j=0;j<=n;j++) {
+                if(i==0||j==0)
+                    dp[i][j] = 0;
+                else if(a.charAt(i) == a.charAt(j))
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        return dp[m][n];
+    }
+
+}
---
+++ src/DP/LongetsCommonSubstring.java
@@ -0,0 +1,31 @@
+package DP;
+
+public class LongetsCommonSubstring {
+
+    // dp[i-1][j-1] adjacent value
+    private int getLongestCommonSubstring(String a, String b) {
+
+        int max= 0 ;
+        int m = a.length();
+        int n = b.length();
+        int[][] dp = new int[m][n];
+
+        for(int i=0;i<m;i++) {
+            for(int j=0;i<n; j++) {
+                if(a.charAt(i)==b.charAt(j)) {
+                    if(i==0|| j==0)
+                        dp[i][j] = 1;
+                    else
+                        dp[i][j] = 1+dp[i-1][j-1];
+
+
+                    if(dp[i][j]>max)
+                        max= dp[i][j];
+                }
+            }
+        }
+        return max;
+
+    }
+
+}
